ad60cbeb19b9b3d54cf462df14c3e3be4e7f7960
Blaj3n <benceulrich@gmail.com>
2024-08-14 14:42:22 +0200

    tömeg vizsgálat for ciklussal

--- fogyokura.py
+++ fogyokura.py
@@ -1,2 +1,19 @@
-cel_het = int(input("Hetek száma="))
-cel_testtomeg = int(input("Elérni kívánt testtömeg (kg)="))
+hetek_szama = int(input("Hetek száma="))
+cel_testtomeg = float(input("Elérni kívánt testtömeg (kg)="))
+tomegek = []
+for het in range(1, hetek_szama+1):
+    tomeg = float(int(input(f"{het}. héten=")))
+    tomegek.append(tomeg)
+
+cel_elert = False
+index = 1
+
+for tomeg in tomegek:
+    if tomeg <= cel_testtomeg:
+        print(f"Mari néni a(z) {index} héten érte el a célt.")
+        cel_elert = True
+        break
+    index += 1
+
+if not cel_elert:
+    print("Sajnos Mari néni nem érte el a célját.")
